510110f5ff166128fe3180ccf28ec91983abd2dd
secf00tprint <secf00tprint@gmail.com>
2024-01-29 19:24:09 +0100

    add option showcpe and possibility to exclude IPs+CVEs

--- README.md
+++ README.md
@@ -11,3 +11,17 @@ Syntax:
 ```
 python3 shodan_grepper.py <ip_file>
 ```
+
+Add a files `excludes.csv` in the same directory
+format:
+
+```
+<ip1>,<cve11>,<cve12>,...
+<ip2>,<cve21>
+<ip3>
+<ip4>,<cve41>
+```
+
+to exclude CVEs from output. For example if you fixed it but Shodan delivers another result.
+
+Add flag `--showcpe` to additionally output found CPEs.
--- shodan_grepper.py
+++ shodan_grepper.py
@@ -1,52 +1,82 @@
 import json
+import argparse
 import os
 import requests
 import sys
 from bs4 import BeautifulSoup
+import csv
 
-shodan_api_url="https://internetdb.shodan.io"
-nist_url="https://nvd.nist.gov/vuln/detail"
+SHODAN_API_URL="https://internetdb.shodan.io"
+NIST_URL="https://nvd.nist.gov/vuln/detail"
+EXCLUDES_FILE="excludes.csv"
 
-if len(sys.argv) < 2:
-    print("You have to add a file containing IPs")
+def get_help_Message():
+    return "python3 shodan_grepper.py <file_containing_ips>"
+
+parser = argparse.ArgumentParser(description="Shodan Grepper")
+parser.add_argument("file_containing_ips", help="You have to add a file containing IPs to check")
+parser.add_argument('--showcpe', action='store_true', help='Show CPE findings if existent')
+
+args = parser.parse_args()
+
+ips_to_exclude=''
+if os.path.exists(EXCLUDES_FILE):
+    with open(EXCLUDES_FILE, newline='\n') as f:
+        reader = csv.reader(f)
+        ips_to_exclude = [row for row in reader]
+
+file_name = sys.argv[1]
+if not os.path.isfile(file_name):
+    print(f"The file {file_name} is not existent")
 else:
-    file_name = sys.argv[1]
-    if os.path.isfile(file_name):
-        with open(file_name,'r') as f:
-            for line in f:
-                ip_to_read = line.strip()
-                url = f'{shodan_api_url}/{ip_to_read}'
-                response = requests.get(url)
-                data = response.json()
-                ip=hostnames=ports=cpes=vulns=''
-                if 'ip' in data:
-                    ip=data['ip']
-                if 'hostnames' in data:
-                    hostnames=data['hostnames']
-                if 'ports' in data:
-                    ports=data['ports']
-                if 'cpes' in data and len(data['cpes'])!=0 or 'vulns' in data and len(data['vulns'])!=0:
-                    print()
-                    print(f'IP: {ip}:{ports} - Hostname(s): {hostnames}')
-                    if 'cpes' in data:
-                        cpes=data['cpes']
-                        print(f'CPEs: {cpes}')
-                    if 'vulns' in data:
-                        vulns=data['vulns']
-                        print(f'Vulns: {vulns}')
-                        for vuln_nr in vulns:
-                            cve_infos_url= f'{nist_url}/{vuln_nr}'
-                            nist_html = requests.get(cve_infos_url)
-                            nist_soup = BeautifulSoup(nist_html.content,'html.parser')
-                            tag = nist_soup.find('a', {'id':'Cvss3NistCalculatorAnchor'})
-                            base_score = str(tag.decode_contents())
-                            colorstart=''
-                            colorend=''
-                            if 'HIGH' in base_score:
-                                colorstart='\033[31m'
-                                colorend='\033[0m'
-                            print(f'{colorstart}{vuln_nr}:{base_score}{colorend}')
-                            print(cve_infos_url)
+    with open(file_name,'r') as f:
+        for line in f:
+            ip_to_read = line.strip()
+            url = f'{SHODAN_API_URL}/{ip_to_read}'
+            response = requests.get(url)
+            data = response.json()
+            ip=hostnames=ports=cpes=vulns=''
+            if 'ip' in data:
+                ip=data['ip']
+            if 'hostnames' in data:
+                hostnames=data['hostnames']
+            if 'ports' in data:
+                ports=data['ports']
+            if 'cpes' in data and len(data['cpes'])!=0 or 'vulns' in data and len(data['vulns'])!=0:
+                # Check if IP is not in the list of IPs to exclude and if so extract the CVEs to exclude
+                exclude_IP = [sublist for sublist in ips_to_exclude if ip in sublist]
+                toprint="\n"
+                toprint=toprint + f"IP: {ip}:{ports} - Hostname(s): {hostnames}\n"
+                if args.showcpe is not False and 'cpes' in data:
+                    cpes=data['cpes']
+                    toprint=toprint+f"CPEs: {cpes}\n"
+                    print(toprint,end='')
+                    toprint=""
+                if 'vulns' in data:
+                    vulns=data['vulns']
+                    toprint=toprint+f"Vulns: {vulns}\n"
+                    for vuln_nr in vulns:
+                        if bool(exclude_IP) and vuln_nr in exclude_IP[0]:
+                            continue
+                        cve_infos_url= f'{NIST_URL}/{vuln_nr}'
+                        nist_html = requests.get(cve_infos_url)
+                        nist_soup = BeautifulSoup(nist_html.content,'html.parser')
+                        tag = nist_soup.find('a', {'id':'Cvss3NistCalculatorAnchor'})
+                        base_score = str(tag.decode_contents())
+                        colorstart=''
+                        colorend=''
+                        if 'CRITICAL' in base_score:
+                            colorstart='\033[31m'
+                        if 'HIGH' in base_score:
+                            colorstart='\033[33m'
+                        if 'MEDIUM' in base_score:
+                            colorstart='\033[93m'
+                        if 'LOW' in base_score:
+                            colorstart='\033[32m'
+                        if bool(colorstart):
+                            colorend='\033[0m'
+                        toprint=toprint+f"{colorstart}{vuln_nr}:{base_score}{colorend}\n"
+                        toprint=toprint+f"{cve_infos_url}\n"
+                        print(toprint,end='')
+                        toprint=""
 
-    else:
-        print(f"The file {file_name} is not existent")
